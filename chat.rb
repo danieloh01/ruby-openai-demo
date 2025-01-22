5b9bdb12ef64318de733ccc220f4607ba3e524b1
danieloh <56809357+danieloh01@users.noreply.github.com>
2025-01-22 21:34:25 +0000

    final commit

--- chat.rb
+++ chat.rb
@@ -21,33 +21,42 @@ client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_KEY"))
 #   }
 # )
 
+message_list = [
+    {
+      "role" => "system",
+      "content" => "You are a very helpful assistant that likes to speak very passionately"
+    }
+  ]
+
 puts "Hello. How can I help you today?"
 puts "-" * 50
 
-user_input = gets.chomp.gsub(" ","%20")
+user_input = ""
 
-message_list = [
-  {
-    "role" => "system",
-    "content" => "You are a very helpful assistant that likes to speak very passionately"
-  },
-  {
-    "role" => "user",
-    "content" => user_input
-  }
-]
+while user_input != "bye"
 
-api_response = client.chat(
-  parameters: {
-    model: "gpt-3.5-turbo",
-    messages: message_list
-  }
-)
+  user_input = gets.chomp.gsub(" ","%20")
+  
+  if user_input != "bye"
+    message_list.push({ "role" => "assistant", "content" => user_input })
 
-specific_1 = api_response.fetch("choices")
-specific_2 = specific_1.at(0)
-specific_3 = specific_2.fetch("message")
-specific_4 = specific_3["content"]
+    api_response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: message_list
+      }
+    )
 
-puts "-" * 50
-pp specific_4
+    specific_1 = api_response.fetch("choices")
+    specific_2 = specific_1.at(0)
+    specific_3 = specific_2.fetch("message")
+    specific_4 = specific_3["content"]
+
+
+    puts "-" * 50
+    pp specific_4
+    
+    message_list.push({ "role" => "assistant", "content" => specific_4 })
+
+  end
+end
